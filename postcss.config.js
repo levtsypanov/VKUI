c2b4a9abfa91a4ba0044e907ada8bc1dcc579538
Vladimir Klepov <v.klepov@gmail.com>
2021-03-18 15:39:03 +0300

    refactor: move constant css vars to vkui__root

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,20 @@
+const path = require('path');
 const cssCustomProperties = require('postcss-custom-properties');
+const scopeRoot = require('./tasks/postcss-scope-root.js');
 const cssImport = require('postcss-import');
 const autoprefixer = require('autoprefixer');
 const selectorPrefixer = require('postcss-prefixer');
 const csso = require('postcss-csso');
+const { defaultSchemeId } = require('./package.json');
 
 let plugins = [
   cssImport(),
   cssCustomProperties({ preserve: true }),
+  // postcss-custom-properties only works with :root
+  scopeRoot({
+    customPropRoot: '.vkui__root, .vkui__portal-root',
+    except: path.resolve(`./src/styles/${defaultSchemeId}.css`)
+  }),
   autoprefixer(),
   selectorPrefixer({
     prefix: 'vkui',
